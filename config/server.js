db7232b2aa85a3ca50dc2b1c4c776e4dba9e051e
rafiq9190 <muhammadrafiq9190@gmail.com>
2021-10-31 03:35:55 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c5db7e9ad4aebabb8b547f4e99a03711'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a4855d52-46d4-421b-af73-774e1c8440fa'
+};
