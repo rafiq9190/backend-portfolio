9fd598817477417cf2bc06603be64bb91bff8263
Muhammad Rafiq <muhammadrafiq017@gmail.com>
2022-03-26 23:07:28 +0500

    files updated

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c5db7e9ad4aebabb8b547f4e99a03711'),
+      secret: env("ADMIN_JWT_SECRET", "c5db7e9ad4aebabb8b547f4e99a03711"),
     },
   },
 });
